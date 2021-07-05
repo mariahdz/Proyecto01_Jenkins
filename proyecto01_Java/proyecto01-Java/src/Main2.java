9f6f1f25095fae9d1d78237ba657c5d259282672
mariahdz <mariahdeztejero@gmail.com>
2021-07-05 10:25:56 +0200

    no se que commit

--- proyecto01_Java/proyecto01-Java/src/Main2.java
+++ proyecto01_Java/proyecto01-Java/src/Main2.java
@@ -3,7 +3,7 @@ public class Main2 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hola chicasss, funciona o quee? no sabemos");
+		System.out.println("Hola chicasss, funciona o quee? no sabemos lo que pasa");
 
 	}
 
